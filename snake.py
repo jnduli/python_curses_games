5d003573114f4aa48edb534608f4331dd3d3a1ff
John Nduli <yohanaizraeli@gmail.com>
2019-02-08 17:31:23 +0300

    Add score board to snake game

--- snake.py
+++ snake.py
@@ -1,34 +1,49 @@
 import random
 import curses
 
-def snake (s):
-    #  s = curses.initscr()
+def initial_snake(width, height):
+    snake_x = width // 4;
+    snake_y = height // 2;
+    return [
+            [snake_y, snake_x],
+            [snake_y, snake_x-1],
+            [snake_y, snake_x-2]
+            ]
+
+def initial_food(width, height):
+    return [height//2, width//2]
+
+def updateScore(scoreWindow, score):
+    scoreMessage = 'Score: {}'.format(score)
+    height, width = scoreWindow.getmaxyx()
+    scoreWindow.addstr(height//2, width//2, scoreMessage)
+    scoreWindow.refresh()
+
+
+def snake(stdscreen):
     curses.curs_set(0)
-    sh, sw = s.getmaxyx()
-    w = curses.newwin(sh, sw, 0, 0)
-    w.keypad(1)
-    w.timeout(100)
+    height, width = stdscreen.getmaxyx()
+    scoreHeight = 4;
+    scoreWindow = curses.newwin(scoreHeight, width, 0, 0)
+    scoreWindow.border(0, 0, 0, 0, 0, 0, 0, 0)
+    window = curses.newwin(height-scoreHeight, width, scoreHeight, 0)
+    window.keypad(1)
+    window.timeout(100)
 
-    snk_x = sw//4
-    snk_y = sh//2
-    snake = [
-        [snk_y, snk_x],
-        [snk_y, snk_x-1],
-        [snk_y, snk_x-2]
-    ]
+    snake = initial_snake(width, height)
+    food = initial_food(width, height)
+    window.addch(food[0], food[1], curses.ACS_PI)
+    updateScore(scoreWindow, 0)
 
-    food = [sh//2, sw//2]
-    w.addch(food[0], food[1], curses.ACS_PI)
-
+    # Intial direction of snake
     key = curses.KEY_RIGHT
-
+    score = 0
     while True:
-        next_key = w.getch()
+        next_key = window.getch()
         key = key if next_key == -1 else next_key
 
-        if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
-            curses.endwin()
-            quit()
+        if snake[0][0] in [0, height] or snake[0][1]  in [0, width] or snake[0] in snake[1:]:
+            return
 
         new_head = [snake[0][0], snake[0][1]]
 
@@ -45,15 +60,19 @@ def snake (s):
 
         if snake[0] == food:
             food = None
+            score = score + 1
+            updateScore(scoreWindow, score)
             while food is None:
                 nf = [
-                    random.randint(1, sh-1),
-                    random.randint(1, sw-1)
+                    random.randint(1, height-1),
+                    random.randint(1, width-1)
                 ]
                 food = nf if nf not in snake else None
-            w.addch(food[0], food[1], curses.ACS_PI)
+            window.addch(food[0], food[1], curses.ACS_PI)
         else:
             tail = snake.pop()
-            w.addch(tail[0], tail[1], ' ')
+            window.addch(tail[0], tail[1], ' ')
 
-        w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
+        window.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
+    return
+
