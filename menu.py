1f33a2edf3f6169d05976ec1871563fec023e351
John Nduli <yohanaizraeli@gmail.com>
2019-02-08 16:26:18 +0300

    Add motion to select items in menu

--- menu.py
+++ menu.py
@@ -6,7 +6,28 @@ def menu(stdscreen):
     display(stdscreen, items)
 
 def display(stdscreen, items, menu_y=5):
+    selected = 0
     while True:
         for index, item in enumerate(items):
-            stdscreen.addstr(menu_y + index, 1, '{}. {}'.format(index,item))
+            if index == selected:
+                mode = curses.A_REVERSE
+            else:
+                mode = curses.A_NORMAL
+            stdscreen.addstr(menu_y + index, 1, '{}. {}'.format(index,item), mode)
+        selected = selected + key_action(stdscreen)
+        if selected < 0:
+            selected = 0
+        if selected >= len(items):
+            selected = len(items)-1
         stdscreen.refresh()
+
+def key_action(stdscreen):
+    key = stdscreen.getch()
+    if key == ord('q'):
+        quit()
+    elif key in [curses.KEY_DOWN, ord('j')]:
+        return 1
+    elif key in [curses.KEY_UP, ord('k')]:
+        return -1
+    else:
+        return 0
