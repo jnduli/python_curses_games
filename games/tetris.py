e8ee5f2b7d1982d5ecc605a38477bc4e716c8d95
John Nduli <yohanaizraeli@gmail.com>
2019-02-13 14:24:36 +0300

    Fix bugs caused by switching to named tuple for car

--- games/tetris.py
+++ games/tetris.py
@@ -32,23 +32,26 @@ def clear_car(window, car):
     paint_car(window, car, ' ')
 
 #TODO: fix this up to prevent unwinable situations
-def generate_villain(hero):
+def generate_villain(hero, villains):
     allowed_x = [1, 5, 9]
-    generate = random.randint(1,9) % 4 == 0
-    if generate:
-        villain = [random.randint(0,20), allowed_x[random.randint(0,2)]]
-        if check_for_collisions(hero, [villain]):
-            return None
-        return villain
-    return None
+    # 20 % change of villain generation
+    generate = random.randint(1,5) % 5 == 0
+    if not generate:
+        return None
+    villain = car(y=random.randint(0,20), x=allowed_x[random.randint(0,2)])
+    if check_for_collisions(hero, [villain]):
+        return None
+    return villain
 
 #TODO: control the speed of motion of villains
 def move_villains(window, villains):
-    for car in villains:
-        clear_car(window, car)
-        car.y = car.y + 1
-        draw_car(window, car)
-    return villains
+    new_villains = []
+    for v in villains:
+        clear_car(window, v)
+        new_v = car(v.y+1, v.x)
+        draw_car(window, new_v)
+        new_villains.append(new_v)
+    return new_villains
 
 def generate_car_bounds(car):
     """
@@ -75,13 +78,14 @@ def check_for_collisions(hero, villains):
                 return True
     return False
 
-def remove_old_cars(window, villains, height):
+def remove_old_cars(window, villains):
+    height,_ = window.getmaxyx()
     new_villains = []
     for car in villains:
-        if car[0] < (height-4):
+        if car.y < (height-4):
             new_villains.append(car)
         else:
-            clear_car(window, *car)
+            clear_car(window, car)
     return new_villains
     
 
@@ -94,28 +98,28 @@ def tetris(stdscreen):
     window.timeout(100)
     window.border(0,0,0,0,0,0,0,0)
     key = 0
-    hero = [height//2, width//3 + 1]
+    hero = car(y=height//2, x=width//3 + 1)
     left_limit = 1 
     right_limit = width - 4
     villains = []
     while key is not ord('q'):
         key = window.getch()
-        villain = generate_villain(hero)
+        villain = generate_villain(hero, villains)
         if villain:
             villains.append(villain)
-        draw_car(window, *hero)
+        draw_car(window, hero)
         new_x = hero_motion(key) * 4 + hero[1]
         if new_x < left_limit:
             new_x = left_limit
         if new_x > right_limit:
             new_x = right_limit
         if hero[1] is not new_x:
-            clear_car(window, *hero)
-            hero[1] = new_x
-        move_villains(window, villains)
+            clear_car(window, hero)
+            hero = car(hero.y, new_x)
+        villains = move_villains(window, villains)
         if (check_for_collisions(hero, villains)):
             return
-        villains = remove_old_cars(window, villains, height)
+        villains = remove_old_cars(window, villains)
 
 def hero_motion(key):
     if key in [curses.KEY_LEFT, ord('h')]:
