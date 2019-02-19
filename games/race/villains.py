bdf6f950a5a5030bd69c2c3d3d3fa0e9a699fba4
John Nduli <yohanaizraeli@gmail.com>
2019-02-19 13:13:13 +0300

    Add collisions as parent of villains

--- games/race/villains.py
+++ games/race/villains.py
@@ -1,18 +1,10 @@
 from collections import deque
 import random
 
+from .collisions import Collision
 from .car import Car
 
-def check_for_collisions(hero, villains):
-    hero_points = hero.bounding_box()
-    for v in villains:
-        for point in hero_points:
-            if v.is_point_in_car(point):
-                return True
-    return False
-
-
-class Villains():
+class Villains(Collision):
     def __init__(self):
         self.villains = deque()
 
@@ -30,12 +22,12 @@ class Villains():
         # 20 % change of villain generation
         generate = random.randint(1,2) % 1 == 0
         villain = Car(y=random.randint(0,5), x=allowed_x[random.randint(0,2)])
-        if not generate and check_for_collisions(hero, [villain]):
+        if not generate and self.check_for_collisions(hero, [villain]):
             return
 
         try:
             last_villain = self.villains[-1]
-            if check_for_collisions(villain, [last_villain]):
+            if self.check_for_collisions(villain, [last_villain]):
                 return
             if (villain.x == 5 or villain.x !=5) and (villain.y + 9) > last_villain.y:
                 return
