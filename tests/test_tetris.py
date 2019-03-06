39c7bca139f3d5cadd33b305ce2714c05d9e4ac9
John Nduli <yohanaizraeli@gmail.com>
2019-03-06 14:58:40 +0300

    Remove unrequired tests

--- tests/test_tetris.py
+++ tests/test_tetris.py
@@ -63,15 +63,3 @@ class TestShapes(unittest.TestCase):
                 Point(7, 5), Point(7, 6)
                 ]
         self.assertEqual(tee.boundingbox, expected_bb)
-
-
-#  class TestTetris(unittest.TestCase):
-#
-    #  def test_rotate_object(self):
-        #  p = point(y=5, x = 10)
-        #  local_zed = zed(p)
-        #  actual_shape, actual_bb = rotate_object(local_zed['shape'], local_zed['boundingbox'])
-        #  expected_bb = [[6.5, 10.5], [4.5, 10.5], [6.5, 11.5], [4.5, 11.5]]
-        #  self.assertEqual(actual_bb, expected_bb)
-        #  expected_shape = [[6.5, 10.5], [5.5, 10.5], [5.5, 11.5], [4.5, 11.5]]
-        #  self.assertEqual(actual_shape, expected_shape)
