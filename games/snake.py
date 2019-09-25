310d9793bef30f4b3cf99d0ca768db4addc6e1bd
saitama <bmunyoki@sibsco.co.ke>
2019-09-25 20:03:09 +0300

    refactor: move Point tuple to game.shapes/ use pylint for python3

--- games/snake.py
+++ games/snake.py
@@ -1,26 +1,24 @@
 import random
 import curses
 
-from collections import namedtuple
-
-Point = namedtuple('Point', ['x_pos', 'y_pos'])
+from games.shapes import Point
 
 
 def initial_snake(width, height):
     snake_x = width // 4
     snake_y = height // 2
     return [
-        Point(x_pos=snake_x, y_pos=snake_y),
-        Point(x_pos=snake_x - 1, y_pos=snake_y),
-        Point(x_pos=snake_x - 2, y_pos=snake_y),
+        Point(x=snake_x, y=snake_y),
+        Point(x=snake_x - 1, y=snake_y),
+        Point(x=snake_x - 2, y=snake_y),
     ]
 
 
 def initial_food(width, height):
-    return Point(x_pos=width // 2, y_pos=height // 2)
+    return Point(x=width // 2, y=height // 2)
 
 
-class Snake(object):
+class Snake:
     SCOREHEIGHT = 4
     MOTIONKEYS = (curses.KEY_DOWN, curses.KEY_UP, curses.KEY_LEFT,
                   curses.KEY_RIGHT, ord('j'), ord('k'), ord('h'), ord('l'))
@@ -32,7 +30,7 @@ class Snake(object):
         self.create_game_window()
         self.snake = initial_snake(self.width, self.height)
         self.food = initial_food(self.width, self.height)
-        self.game_window.addch(self.food.y_pos, self.food.x_pos, curses.ACS_PI)
+        self.game_window.addch(self.food.y, self.food.x, curses.ACS_PI)
         self.update_score(0)
 
     def create_score_window(self):
@@ -52,26 +50,26 @@ class Snake(object):
         self.score_window.refresh()
 
     def snake_beyond_boundaries_or_hit_itself(self):
-        return (self.snake[0].y_pos in [0, self.height]
-                or self.snake[0].x_pos in [0, self.width]
+        return (self.snake[0].y in [0, self.height]
+                or self.snake[0].x in [0, self.width]
                 or self.snake[0] in self.snake[1:])
 
     def move_snake(self, key):
-        new_head = Point(x_pos=self.snake[0].x_pos, y_pos=self.snake[0].y_pos)
+        new_head = Point(x=self.snake[0].x, y=self.snake[0].y)
         if key in [curses.KEY_DOWN, ord('j')]:
-            new_head = new_head._replace(y_pos=new_head.y_pos + 1)
+            new_head = new_head._replace(y=new_head.y + 1)
         if key in [curses.KEY_UP, ord('k')]:
-            new_head = new_head._replace(y_pos=new_head.y_pos - 1)
+            new_head = new_head._replace(y=new_head.y - 1)
         if key in [curses.KEY_LEFT, ord('h')]:
-            new_head = new_head._replace(x_pos=new_head.x_pos - 1)
+            new_head = new_head._replace(x=new_head.x - 1)
         if key in [curses.KEY_RIGHT, ord('l')]:
-            new_head = new_head._replace(x_pos=new_head.x_pos + 1)
+            new_head = new_head._replace(x=new_head.x + 1)
         self.snake.insert(0, new_head)
 
     def new_food(self):
         while True:
-            new_food = Point(x_pos=random.randint(1, self.width - 1),
-                             y_pos=random.randint(
+            new_food = Point(x=random.randint(1, self.width - 1),
+                             y=random.randint(
                                  1, self.height - self.SCOREHEIGHT - 1))
             if new_food not in self.snake:
                 return new_food
@@ -107,13 +105,12 @@ class Snake(object):
             if self.eat_food():
                 score = score + 1
                 self.update_score(score)
-                self.game_window.addch(self.food.y_pos, self.food.x_pos,
-                                       curses.ACS_PI)
+                self.game_window.addch(self.food.y, self.food.x, curses.ACS_PI)
             else:
                 tail = self.snake.pop()
-                self.game_window.addch(tail.y_pos, tail.x_pos, ' ')
-                self.game_window.addch(self.snake[0].y_pos,
-                                       self.snake[0].x_pos, curses.ACS_CKBOARD)
+                self.game_window.addch(tail.y, tail.x, ' ')
+                self.game_window.addch(self.snake[0].y, self.snake[0].x,
+                                       curses.ACS_CKBOARD)
 
 
 def snake_game(stdscreen):
